430ceb1e4615f9e44ecc7f6f51348975ace56fdf
mohamadtarabulsi <salem.t@hotmail.com>
2023-04-14 01:21:00 -0500

    Finished Capstone Project

--- Capstone/FinalProject/src/main/java/com/kenzie/app/Application.java
+++ Capstone/FinalProject/src/main/java/com/kenzie/app/Application.java
@@ -1,6 +1,11 @@
 package com.kenzie.app;
 
 // import necessary libraries
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     /* Java Fundamentals Capstone project:
@@ -14,7 +19,71 @@ public class Application {
      */
 
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         //Write main execution code here
+        // define API URL
+        String URL = "https://jservice.kenzie.academy/api/clues";
+
+        // Send GET request to the API to fetch data and store the response
+        String jsonResponse = CustomHttpClient.sendGET(URL);
+
+        // Convert the JSON response to a list of Clue objects
+        List<Clue> clues = parseClues(jsonResponse);
+
+        if (clues == null) {
+            System.out.println("Error: Unable to fetch clues from the API.");
+            return;
+        }
+
+        // Shuffle clues randomly
+        Collections.shuffle(clues);
+
+        // Initialize score and total questions
+        int score = 0;
+        int totalQuestions = 10;
+        Scanner scanner = new Scanner(System.in);
+
+        // Loop through the number of questions
+        for (int i = 0; i < totalQuestions; i++) {
+            Clue clue = clues.get(i);
+            Category category = clue.getCategory();
+            // Display the category and question
+            System.out.println("Category: " + category.getTitle());
+            System.out.println("Question: " + clue.getQuestion());
+            // Read user input
+            System.out.print("Your answer: ");
+            String userAnswer = scanner.nextLine();
+            // Check if user's answer is correct
+            boolean isCorrect = isAnswerCorrect(userAnswer, clue.getAnswer());
+            if (isCorrect) {
+                score++;
+                System.out.println("Great work! Your answer is correct.");
+            } else {
+                System.out.println("Sorry! That answer was incorrect. The correct answer is \"" + clue.getAnswer() + "\".");
+            }
+            // Display current score
+            System.out.println("Current Score: " + score);
+        }
+        // Display final score
+        System.out.println("Game over! Your final score is: " + score);
+        scanner.close();
+    }
+    // Method to parse clues from JSOn response
+    private static List<Clue> parseClues(String responseBody) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ApiResponse apiResponse = objectMapper.readValue(responseBody, ApiResponse.class);
+            return apiResponse.getClues();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    // Method to check if user's answer is correct
+    // The method is not case sensitive, ignores leading, trailing spaces, and ignores apostrophes
+    private static boolean isAnswerCorrect(String userAnswer, String correctAnswer) {
+        String normalizedUserAnswer = userAnswer.trim().toLowerCase().replaceAll("'", "");
+        String normalizedCorrectAnswer = correctAnswer.trim().toLowerCase().replaceAll("'", "");
+        return normalizedUserAnswer.equals(normalizedCorrectAnswer);
     }
 }
--- Capstone/FinalProject/src/main/java/com/kenzie/app/CustomHttpClient.java
+++ Capstone/FinalProject/src/main/java/com/kenzie/app/CustomHttpClient.java
@@ -4,13 +4,33 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.io.IOException;
 
+// Define the CustomHttpClient class
 public class CustomHttpClient {
 
     //TODO: Write sendGET method that takes URL and returns response
     public static String sendGET(String URLString) {
         //** Start of GET request algorithm
+        // Create an HttpClient with default settings and HTTP/1.1 protocol version
+        HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
+        // Create an HttpRequest with the provided URL and GET method
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(URLString)).GET().build();
 
-        return "";
+        try {
+            // Send the request and receive the response as a String
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            // Check if the status code is 200 (success), return the response body, else print an error message
+            if (response.statusCode() == 200) {
+                return response.body();
+            } else {
+                System.out.println("Error while fetching data from the API. Status code: " + response.statusCode());
+                return "";
+            }
+        } catch (IOException | InterruptedException e) {
+            // Catch exceptions, print an error message, and return an empty string
+            System.out.println("Error while sending GET request: " + e.getMessage());
+            return "";
+        }
     }
 }
